398cab8f94a8a49a76ce6dbd21c7e08436c2e845
BrianMassick <bmassick@northislandcollege.ca>
2023-03-22 12:46:25 -0700

    Feature: added more outfit options

--- itemBank/outfits.js
+++ itemBank/outfits.js
@@ -2,5 +2,11 @@ export const OUTFITS = [
     'bunny-basic',
     'bunny-cute',
     'fox-basic',
-    'fox-cute'
+    'fox-cute',
+    'cat-basic',
+    'cat-cute',
+    'raccoon-basic',
+    'raccoon-cute',
+    'squirrel-basic',
+    'squirrel-cute'
 ]
